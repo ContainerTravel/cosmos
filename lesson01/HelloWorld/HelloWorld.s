887690473d9a8cad28b55ab7f1a9932f5d980476
LMOS <609379965@qq.com>
2021-05-06 16:15:24 +0800

    程序的运行过程:从代码到机器运行

---
+++ lesson01/HelloWorld/HelloWorld.s
@@ -0,0 +1,30 @@
+	.file	"HelloWorld.c"
+	.text
+	.section	.rodata
+.LC0:
+	.string	"hello world!"
+	.text
+	.globl	main
+	.type	main, @function
+main:
+.LFB0:
+	.cfi_startproc
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	subq	$16, %rsp
+	movl	%edi, -4(%rbp)
+	movq	%rsi, -16(%rbp)
+	leaq	.LC0(%rip), %rdi
+	call	puts@PLT
+	movl	$0, %eax
+	leave
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE0:
+	.size	main, .-main
+	.ident	"GCC: (Debian 7.3.0-19) 7.3.0"
+	.section	.note.GNU-stack,"",@progbits
