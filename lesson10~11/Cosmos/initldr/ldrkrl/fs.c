27ebfb7e8bc0a67fff078d224e64de1a4988614c
LMOS <609379965@qq.com>
2021-05-29 18:02:03 +0800

    第10、11课代码

---
+++ lesson10~11/Cosmos/initldr/ldrkrl/fs.c
@@ -0,0 +1,215 @@
+#include "cmctl.h"
+
+void fs_entry()
+{
+    return;
+}
+
+int strcmpl(const char *a, const char *b)
+{
+
+    while (*b && *a && (*b == *a))
+    {
+
+        b++;
+
+        a++;
+    }
+
+    return *b - *a;
+}
+
+fhdsc_t *get_fileinfo(char_t *fname, machbstart_t *mbsp)
+{
+
+    mlosrddsc_t *mrddadrs = (mlosrddsc_t *)((u32_t)(mbsp->mb_imgpadr + MLOSDSC_OFF));
+    if (mrddadrs->mdc_endgic != MDC_ENDGIC ||
+        mrddadrs->mdc_rv != MDC_RVGIC ||
+        mrddadrs->mdc_fhdnr < 2 ||
+        mrddadrs->mdc_filnr < 2)
+    {
+        kerror("no mrddsc");
+    }
+
+    s64_t rethn = -1;
+    fhdsc_t *fhdscstart = (fhdsc_t *)((u32_t)(mrddadrs->mdc_fhdbk_s) + ((u32_t)(mbsp->mb_imgpadr)));
+
+    for (u64_t i = 0; i < mrddadrs->mdc_fhdnr; i++)
+    {
+        if (strcmpl(fname, fhdscstart[i].fhd_name) == 0)
+        {
+            rethn = (s64_t)i;
+            goto ok_l;
+        }
+    }
+    rethn = -1;
+ok_l:
+    if (rethn < 0)
+    {
+        kerror("not find file");
+    }
+    return &fhdscstart[rethn];
+}
+
+int move_krlimg(machbstart_t *mbsp, u64_t cpyadr, u64_t cpysz)
+{
+
+    if (0xffffffff <= (cpyadr + cpysz) || 1 > cpysz)
+    {
+        return 0;
+    }
+    void *toadr = (void *)((u32_t)(P4K_ALIGN(cpyadr + cpysz)));
+    sint_t tosz = (sint_t)mbsp->mb_imgsz;
+    if (0 != adrzone_is_ok(mbsp->mb_imgpadr, mbsp->mb_imgsz, cpyadr, cpysz))
+    {
+        if (NULL == chk_memsize((e820map_t *)((u32_t)(mbsp->mb_e820padr)), (u32_t)mbsp->mb_e820nr, (u64_t)((u32_t)toadr), (u64_t)tosz))
+        {
+            return -1;
+        }
+        m2mcopy((void *)((u32_t)mbsp->mb_imgpadr), toadr, tosz);
+        mbsp->mb_imgpadr = (u64_t)((u32_t)toadr);
+        return 1;
+    }
+    return 2;
+}
+
+void init_krlfile(machbstart_t *mbsp)
+{
+    u64_t sz = r_file_to_padr(mbsp, IMGKRNL_PHYADR, "kernel.bin");
+    if (0 == sz)
+    {
+        kerror("r_file_to_padr err");
+    }
+    mbsp->mb_krlimgpadr = IMGKRNL_PHYADR;
+    mbsp->mb_krlsz = sz;
+    mbsp->mb_nextwtpadr = P4K_ALIGN(mbsp->mb_krlimgpadr + mbsp->mb_krlsz);
+    mbsp->mb_kalldendpadr = mbsp->mb_krlimgpadr + mbsp->mb_krlsz;
+    return;
+}
+
+void init_defutfont(machbstart_t *mbsp)
+{
+    u64_t sz = 0;
+    u32_t dfadr = (u32_t)mbsp->mb_nextwtpadr;
+    sz = r_file_to_padr(mbsp, dfadr, "font.fnt");
+    if (0 == sz)
+    {
+        kerror("r_file_to_padr err");
+    }
+    mbsp->mb_bfontpadr = (u64_t)(dfadr);
+    mbsp->mb_bfontsz = sz;
+    mbsp->mb_nextwtpadr = P4K_ALIGN((u32_t)(dfadr) + sz);
+    mbsp->mb_kalldendpadr = mbsp->mb_bfontpadr + mbsp->mb_bfontsz;
+    return;
+}
+
+void get_file_rpadrandsz(char_t *fname, machbstart_t *mbsp, u32_t *retadr, u32_t *retsz)
+{
+    u64_t padr = 0, fsz = 0;
+    if (NULL == fname || NULL == mbsp)
+    {
+        *retadr = 0;
+        return;
+    }
+    fhdsc_t *fhdsc = get_fileinfo(fname, mbsp);
+    if (fhdsc == NULL)
+    {
+        *retadr = 0;
+        return;
+    }
+    padr = fhdsc->fhd_intsfsoff + mbsp->mb_imgpadr;
+    if (padr > 0xffffffff)
+    {
+        *retadr = 0;
+        return;
+    }
+    fsz = (u32_t)fhdsc->fhd_frealsz;
+    if (fsz > 0xffffffff)
+    {
+        *retadr = 0;
+        return;
+    }
+    *retadr = (u32_t)padr;
+    *retsz = (u32_t)fsz;
+    return;
+}
+
+u64_t get_filesz(char_t *filenm, machbstart_t *mbsp)
+{
+    if (filenm == NULL || mbsp == NULL)
+    {
+        return 0;
+    }
+    fhdsc_t *fhdscstart = get_fileinfo(filenm, mbsp);
+    if (fhdscstart == NULL)
+    {
+        return 0;
+    }
+    return fhdscstart->fhd_frealsz;
+}
+
+u64_t get_wt_imgfilesz(machbstart_t *mbsp)
+{
+    u64_t retsz = LDRFILEADR;
+    mlosrddsc_t *mrddadrs = MRDDSC_ADR;
+    if (mrddadrs->mdc_endgic != MDC_ENDGIC ||
+        mrddadrs->mdc_rv != MDC_RVGIC ||
+        mrddadrs->mdc_fhdnr < 2 ||
+        mrddadrs->mdc_filnr < 2)
+    {
+        return 0;
+    }
+    if (mrddadrs->mdc_filbk_e < 0x4000)
+    {
+        return 0;
+    }
+    retsz += mrddadrs->mdc_filbk_e;
+    retsz -= LDRFILEADR;
+    mbsp->mb_imgpadr = LDRFILEADR;
+    mbsp->mb_imgsz = retsz;
+    return retsz;
+}
+
+u64_t r_file_to_padr(machbstart_t *mbsp, u32_t f2adr, char_t *fnm)
+{
+    if (NULL == f2adr || NULL == fnm || NULL == mbsp)
+    {
+        return 0;
+    }
+    u32_t fpadr = 0, sz = 0;
+    get_file_rpadrandsz(fnm, mbsp, &fpadr, &sz);
+    if (0 == fpadr || 0 == sz)
+    {
+        return 0;
+    }
+    if (NULL == chk_memsize((e820map_t *)((u32_t)mbsp->mb_e820padr), (u32_t)(mbsp->mb_e820nr), f2adr, sz))
+    {
+        return 0;
+    }
+    if (0 != chkadr_is_ok(mbsp, f2adr, sz))
+    {
+        return 0;
+    }
+    m2mcopy((void *)fpadr, (void *)f2adr, (sint_t)sz);
+    return sz;
+}
+
+u64_t ret_imgfilesz()
+{
+    u64_t retsz = LDRFILEADR;
+    mlosrddsc_t *mrddadrs = MRDDSC_ADR;
+    if (mrddadrs->mdc_endgic != MDC_ENDGIC ||
+        mrddadrs->mdc_rv != MDC_RVGIC ||
+        mrddadrs->mdc_fhdnr < 2 ||
+        mrddadrs->mdc_filnr < 2)
+    {
+        kerror("no mrddsc");
+    }
+    if (mrddadrs->mdc_filbk_e < 0x4000)
+    {
+        kerror("imgfile error");
+    }
+    retsz += mrddadrs->mdc_filbk_e;
+    retsz -= LDRFILEADR;
+    return retsz;
+}
---
+++ lesson10~11/Cosmos/initldr/ldrkrl/vgastr.c
@@ -0,0 +1,177 @@
+/**********************************************************
+		转换显示字符串文件vgastr.c
+***********************************************************
+				彭东 ＠ 2011.12.15.07.30
+**********************************************************/
+
+#include "cmctl.h"
+__attribute__((section(".data"))) cursor_t curs;
+
+void init_curs()
+{
+
+    curs.vmem_s = VGASTR_RAM_BASE;
+    curs.vmem_e = VGASTR_RAM_END;
+    curs.cvmemadr = 0;
+    curs.x = 0;
+    curs.y = 0;
+    return;
+}
+
+void _strwrite(char_t *str, cursor_t *cursptr)
+{
+
+    uint_t straddr = cursptr->x + cursptr->y * 80 * 2;
+    char_t *p_strdst = (char_t *)(cursptr->vmem_s + straddr);
+    u32_t tfindx = FALSE;
+
+    while (*str)
+    {
+
+        if (*str == 10)
+        {
+            tfindx = TRUE;
+            str++;
+            if (*str == 0)
+            {
+                break;
+            }
+        }
+
+        current_curs(cursptr, VGACHAR_DF_CFLG);
+
+        *p_strdst = *str++;
+        p_strdst += 2;
+    }
+
+    if (tfindx == TRUE)
+    {
+        current_curs(cursptr, VGACHAR_LR_CFLG);
+    }
+
+    return;
+}
+
+char_t *numberk(char_t *str, uint_t n, sint_t base)
+{
+    register char_t *p;
+    char_t strbuf[36];
+    p = &strbuf[36];
+    *--p = 0;
+
+    if (n == 0)
+    {
+        *--p = '0';
+    }
+
+    else
+    {
+        do
+        {
+            *--p = "0123456789abcdef"[n % base];
+        } while (n /= base);
+    }
+    while (*p != 0)
+    {
+        *str++ = *p++;
+    }
+    return str;
+}
+
+void set_curs(u32_t x, u32_t y)
+{
+    curs.x = x;
+    curs.y = y;
+    return;
+}
+
+void clear_screen(u16_t srrv)
+{
+    curs.x = 0;
+    curs.y = 0;
+
+    u16_t *p = (u16_t *)VGASTR_RAM_BASE;
+
+    for (uint_t i = 0; i < 2001; i++)
+    {
+        p[i] = srrv;
+    }
+
+    close_curs();
+    return;
+}
+
+void put_one_char(char_t cr, uint_t x, uint_t y)
+{
+
+    char_t *p_strdst = (char_t *)(VGASTR_RAM_BASE + (x + (y * 80 * 2)));
+    *p_strdst = cr;
+    return;
+}
+
+void close_curs()
+{
+    out_u8(VGACTRL_REG_ADR, VGACURS_REG_INX);
+    out_u8(VGACTRL_REG_DAT, VGACURS_CLOSE);
+    return;
+}
+
+char_t *strcopy(char_t *buf, char_t *str_s)
+{
+    while (*str_s)
+    {
+        *buf = *str_s;
+        buf++;
+        str_s++;
+    }
+    return buf;
+}
+
+void vsprintfk(char_t *buf, const char_t *fmt, va_list_t args)
+{
+    char_t *p = buf;
+    va_list_t next_arg = args;
+    while (*fmt)
+    {
+        if (*fmt != '%')
+        {
+            *p++ = *fmt++;
+            continue;
+        }
+        fmt++;
+        switch (*fmt)
+        {
+        case 'x':
+            p = numberk(p, *((long *)next_arg), 16);
+            next_arg += sizeof(long);
+            fmt++;
+
+            break;
+        case 'd':
+            p = numberk(p, *((long *)next_arg), 10);
+            next_arg += sizeof(long);
+            fmt++;
+            break;
+        case 's':
+            p = strcopy(p, (char_t *)(*((long *)next_arg)));
+            next_arg += sizeof(long);
+            fmt++;
+            break;
+        default:
+            break;
+        }
+    }
+    *p = 0;
+    return;
+}
+
+void kprint(const char_t *fmt, ...)
+{
+    char_t buf[512];
+
+    va_list_t arg = (va_list_t)((char_t *)(&fmt) + sizeof(long));
+
+    vsprintfk(buf, fmt, arg);
+    _strwrite(buf, &curs);
+    return;
+}
