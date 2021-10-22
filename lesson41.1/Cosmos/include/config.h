b57b180692220a668fbd404b6c9f206d540d46f7
LMOS <609379965@qq.com>
2021-10-22 16:38:05 +0800

    修正config.h文件

--- lesson41.1/Cosmos/include/config.h
+++ lesson41.1/Cosmos/include/config.h
@@ -5,7 +5,8 @@
 ****************************************************************/
 #ifndef _CONFIG_H
 #define _CONFIG_H
-
 #define CFG_X86_PLATFORM
+#ifdef CFG_X86_PLATFORM
 #define APPRUN_START_VITRUALADDR (0x100000)
+#endif
 #endif
